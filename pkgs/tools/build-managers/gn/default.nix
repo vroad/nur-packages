d87ece6c30f1c084a9e2e2ec33ec76893b2de3a4
vroad <396351+vroad@users.noreply.github.com>
2021-04-11 22:52:09 +0900

    gn: Update gn to 2021-04-06

--- pkgs/tools/build-managers/gn/default.nix
+++ pkgs/tools/build-managers/gn/default.nix
@@ -5,10 +5,10 @@
 let
   # Note: Please use the recommended version for Chromium, e.g.:
   # https://git.archlinux.org/svntogit/packages.git/tree/trunk/chromium-gn-version.sh?h=packages/gn
-  rev = "fd3d768bcfd44a8d9639fe278581bd9851d0ce3a";
-  revNum = "1718"; # git describe HEAD --match initial-commit | cut -d- -f3
-  version = "2020-03-09";
-  sha256 = "1asc14y8by7qcn10vbk467hvx93s30pif8r0brissl0sihsaqazr";
+  rev = "dba01723a441c358d843a575cb7720d54ddcdf92";
+  revNum = "1897"; # git describe HEAD --match initial-commit | cut -d- -f3
+  version = "2021-04-06";
+  sha256 = "199xkks67qrn0xa5fhp24waq2vk8qb78a96cb3kdd8v1hgacgb8x";
 
   revShort = builtins.substring 0 7 rev;
   lastCommitPosition = writeText "last_commit_position.h" ''
