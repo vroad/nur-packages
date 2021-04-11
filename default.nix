15f2b9b11d20d735900775aab09bd6f631d84826
vroad <396351+vroad@users.noreply.github.com>
2021-04-11 22:47:31 +0900

    gn: Add gn

--- default.nix
+++ default.nix
@@ -17,4 +17,5 @@
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
+  gn = pkgs.callPackage ./pkgs/tools/build-managers/gn { };
 }
