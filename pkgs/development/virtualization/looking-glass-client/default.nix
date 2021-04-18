120ddebf4501b307ca3b991fc74994c325733124
vroad <396351+vroad@users.noreply.github.com>
2021-04-18 14:37:50 +0900

    Update looking-glass-client to bleeding edge version

--- pkgs/development/virtualization/looking-glass-client/default.nix
+++ pkgs/development/virtualization/looking-glass-client/default.nix
@@ -1,18 +1,18 @@
 # https://github.com/NixOS/nixpkgs/blob/38cef0ba3b4c215cf1d4d03d137c66d948bdf1be/pkgs/applications/virtualization/looking-glass-client/default.nix
 { lib, stdenv, fetchFromGitHub, cmake, pkg-config, SDL2, SDL2_ttf, spice-protocol
-, fontconfig, libX11, freefont_ttf, nettle, libpthreadstubs, libXau, libXdmcp
-, libXi, libXext, wayland, libffi, libGLU, expat, libbfd
+, fontconfig, libX11, freefont_ttf, nettle, libXau, libXdmcp, libXi, libXext
+, wayland, libffi, expat, libbfd, libXScrnSaver, wayland-protocols, libXinerama
 }:
 
 stdenv.mkDerivation rec {
   pname = "looking-glass-client";
-  version = "B2";
+  version = "ae36abb1ca3a645ac816667d6290121ac641cb01";
 
   src = fetchFromGitHub {
     owner = "gnif";
     repo = "LookingGlass";
     rev = version;
-    sha256 = "100b5kzh8gr81kzw5fdqz2jsms25hv3815d31vy3qd6lrlm5gs3d";
+    sha256 = "1cgcnvs7kbqcn3sczl09js58q7y9lmq4w61wh2zq3s8p30144f9s";
     fetchSubmodules = true;
   };
 
@@ -20,12 +20,12 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     SDL2 SDL2_ttf spice-protocol fontconfig libX11 freefont_ttf nettle
-    libpthreadstubs libXau libXdmcp libXi libXext wayland libffi libGLU expat
-    libbfd
+    libXau libXdmcp libXi libXext wayland libffi expat libbfd libXScrnSaver
+    wayland-protocols libXinerama
   ];
 
   sourceRoot = "source/client";
-  NIX_CFLAGS_COMPILE = "-mavx"; # Fix some sort of AVX compiler problem.
+  NIX_CFLAGS_COMPILE = "-mavx -Wno-uninitialized"; # Fix some sort of AVX compiler problem and avoid false-positive gcc warnings.
 
   meta = with lib; {
     description = "A KVM Frame Relay (KVMFR) implementation";
