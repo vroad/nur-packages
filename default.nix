079e8844cf6c7df9733ea0d0d7a6dd542fda3001
vroad <396351+vroad@users.noreply.github.com>
2021-04-18 17:46:40 +0900

    Use rec

--- default.nix
+++ default.nix
@@ -1,8 +1,5 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
-let
+rec {
   gn = pkgs.callPackage ./pkgs/development/tools/build-managers/gn { };
   depot-tools = pkgs.callPackage ./pkgs/development/tools/depot-tools { };
-in
-{
-  inherit depot-tools gn;
 }
