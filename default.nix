9d9cb76815a22dbbaf26e52d827937878c897046
vroad <396351+vroad@users.noreply.github.com>
2021-04-30 10:43:41 +0900

    Add qemu from nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,13 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
+with pkgs;
 {
   modules = import ./modules; # NixOS modules
 
-  looking-glass-client = pkgs.callPackage ./pkgs/development/virtualization/looking-glass-client { };
+  qemu = callPackage ./pkgs/applications/virtualization/qemu {
+    inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
+    inherit (darwin.stubs) rez setfile;
+    python = python3;
+  };
+
+  looking-glass-client = callPackage ./pkgs/development/virtualization/looking-glass-client { };
 }
