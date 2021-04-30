d9389a75e869d2733cc8dcb5919aea10ba30096d
vroad <396351+vroad@users.noreply.github.com>
2021-04-30 10:37:37 +0900

    Remove gn and depot-tools

--- default.nix
+++ default.nix
@@ -2,7 +2,5 @@
 {
   modules = import ./modules; # NixOS modules
 
-  gn = pkgs.callPackage ./pkgs/development/tools/build-managers/gn { };
-  depot-tools = pkgs.callPackage ./pkgs/development/tools/depot-tools { };
   looking-glass-client = pkgs.callPackage ./pkgs/development/virtualization/looking-glass-client { };
 }
