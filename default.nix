7803d2ed4e8c56552e57b1cff531b70df4948b49
vroad <396351+vroad@users.noreply.github.com>
2021-05-01 11:51:04 +0900

    qemu: Add qemu_kvm variant

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
-with pkgs;
-{
-  modules = import ./modules; # NixOS modules
-
-  qemu = callPackage ./pkgs/applications/virtualization/qemu {
+let
+  qemu = with pkgs; callPackage ./pkgs/applications/virtualization/qemu {
     inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
     inherit (darwin.stubs) rez setfile;
     python = python3;
   };
+in
+{
+  modules = import ./modules; # NixOS modules
 
-  looking-glass-client = callPackage ./pkgs/development/virtualization/looking-glass-client { };
+  inherit qemu;
+  qemu_kvm = pkgs.lowPrio (qemu.override { hostCpuOnly = true; });
+
+  looking-glass-client = pkgs.callPackage ./pkgs/development/virtualization/looking-glass-client { };
 }
