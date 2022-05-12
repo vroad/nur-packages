c523e23b6512c0fbb380f89b3e48c51212e0ac34
vroad <396351+vroad@users.noreply.github.com>
2022-05-12 11:46:00 +0900

    Remove qemu

--- default.nix
+++ default.nix
@@ -1,18 +1,6 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
-let
-  qemu = with pkgs; callPackage ./pkgs/applications/virtualization/qemu {
-    inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
-    inherit (darwin.stubs) rez setfile;
-    python = python3;
-  };
-in
 {
   modules = import ./modules; # NixOS modules
-
-  qemu = qemu.overrideAttrs (x: {
-    preferLocalBuild = true;
-  });
-  qemu_kvm = pkgs.lowPrio (qemu.override { hostCpuOnly = true; });
 
   looking-glass-client = pkgs.callPackage ./pkgs/development/virtualization/looking-glass-client { };
 }
