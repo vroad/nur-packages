0424336d54f48fa82b906aeaa3bd8d6dc8477ec4
vroad <396351+vroad@users.noreply.github.com>
2022-05-12 11:58:47 +0900

    Update looking-glass-client

--- default.nix
+++ default.nix
@@ -1,6 +1,20 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+  unstablePkgs = import sources.nixpkgs-unstable { };
+in
 {
   modules = import ./modules; # NixOS modules
 
-  looking-glass-client = pkgs.callPackage ./pkgs/development/virtualization/looking-glass-client { };
+  looking-glass-client =
+    unstablePkgs.looking-glass-client.overrideAttrs (oldAttrs: rec {
+      version = "3df7d30cd5b551b23e41781313cef389239e27bc";
+      src = unstablePkgs.fetchFromGitHub {
+        owner = "gnif";
+        repo = "LookingGlass";
+        rev = version;
+        sha256 = "sha256-UzZQU5SzJ2mo9QBweQB0VJSnKfzgTG5QaKpIQN/6LCE=";
+        fetchSubmodules = true;
+      };
+    });
 }
