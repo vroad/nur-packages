8050762ad0032cddf19b3e460e374da295ad84e2
vroad <396351+vroad@users.noreply.github.com>
2022-06-23 16:08:11 +0900

    Re-add qemu

--- default.nix
+++ default.nix
@@ -1,7 +1,15 @@
 { ... }:
 let
   sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  qemu = with pkgs; callPackage ./pkgs/applications/virtualization/qemu {
+    inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
+    inherit (darwin.stubs) rez setfile;
+    inherit (darwin) sigtool;
+  };
 in
 {
   modules = import ./modules; # NixOS modules
+
+  qemu_kvm = pkgs.lowPrio (qemu.override { hostCpuOnly = true; });
 }
